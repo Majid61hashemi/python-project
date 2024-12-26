51c7b16561961f157fc7ec4081fd67763152d515
MajidHashemi <hashemimajid42@gmail.com>
2024-12-26 10:18:06 +0330

    feat: word frequency update

--- 04-WordFrequencies.py
+++ 04-WordFrequencies.py
@@ -1,30 +1,30 @@
-import os
+import os 
 import string
 
 def read_file(file_path):
+    # Read and return the contents of a file
     if not os.path.exists(file_path):
         raise FileNotFoundError(f"The file at '{file_path}' does not exist.")
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
 
-def split_line(line):
-    return line.replace('-', ' ').split()
-
 def clean_text(text):
+    # Clean text by removing punctuation .
     translator = str.maketrans('', '', string.punctuation)
     words = text.translate(translator).split()
-    return words.lower()
+    filtered_words =[word.lower() for  word in words]
+    return filtered_words
 
 input_path = input("Enter input file path: ").strip()
+# Read files
 text = read_file(input_path)
-
-for line in open(input_path):
-        print(clean_text(line))
+# Analyze rext
+words = clean_text(text)
+lines = text.splitlines()
+sentences = text.split('.')
 
 word_counter = {}
-for line in open(input_path):
-    for word in split_line(line):
-        word = clean_text(word)
+for word in words:
         if word not in word_counter:
             word_counter[word] = 1
         else:
@@ -32,10 +32,11 @@ for line in open(input_path):
 
 def second_element(t):
     return t[1]
+
 def print_most_common(word_counter, num=5):
     items = sorted(word_counter.items(), key=second_element, reverse=True)
 
     for word, freq in items[:num]:
         print(freq, word, sep='\t')
 
-print_most_common(word_counter, 3)
+print_most_common(word_counter, 6)
